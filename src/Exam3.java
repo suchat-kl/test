e6c3b01c5518173cd20f7c4f03d954d5e887b021
suchat-kl <60504254+suchat-kl@users.noreply.github.com>
2021-05-24 17:16:55 +0700

    upload#1

---
+++ src/Exam3.java
@@ -0,0 +1,26 @@
+
+public class Exam3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 20;
+		i++;
+		i++;
+		i++;
+		i++;
+		i++;
+		System.out.println("i value is " + i);
+		--i;
+		--i;
+		--i;
+		--i;
+		--i;
+		System.out.println("i value is " + i);
+
+		float f1 = 1.2F, f2 = 1.3F;
+		System.out.println("f1 && f2 " + (f1 && f2));
+		char c1 = 'A', c2 = 'B';
+		System.out.println("c1 || c2" + (c1 || c2));
+	}
+
+}
